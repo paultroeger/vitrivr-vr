d7a763bae690d7b90303f6920d9ef36cb1515cea
Paul Troeger <hallaskog@gmail.com>
2023-04-25 12:50:33 +0200

    Added Advance Grid with scrollbar and first test mediaItem

--- Assets/Scripts/VitrivrVR/Query/Display/AdvancedGridQueryDisplay.cs
+++ Assets/Scripts/VitrivrVR/Query/Display/AdvancedGridQueryDisplay.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -13,47 +14,111 @@ using static VitrivrVR.Logging.Interaction;
 
 namespace VitrivrVR.Query.Display
 {
-    /// <summary>
-    /// Displays queries in an advanced grid.
-    /// </summary>
-    public class AdvancedGridQueryDisplay : QueryDisplay
+  /// <summary>
+  /// Displays queries in an advanced grid.
+  /// </summary>
+  public class AdvancedGridQueryDisplay : QueryDisplay
+  {
+
+    public override int NumberOfResults => _nResults;
+    public MediaItemDisplay mediaItemDisplay;
+
+    private int _nResults;
+    private readonly List<MediaItemDisplay> _mediaDisplays = new();
+
+
+    protected override void Initialize()
     {
-
-      public override int NumberOfResults => -6;
-      
-      protected override void Initialize()
+      var fusionResults = QueryData.GetMeanFusionResults();
+      var _results = fusionResults;
+      if (_results == null)
       {
-        var fusionResults = QueryData.GetMeanFusionResults();
-        var _results = fusionResults;
-        if (_results == null)
-        {
-            NotificationController.Notify("No results returned from query!");
-            _results = new List<ScoredSegment>();
-        }
-
-        gameObject.AddComponent<Canvas>();
-
-        Canvas canvas = GetComponent<Canvas>();
-        canvas.GetComponent<RectTransform>().sizeDelta = new Vector2(150, 200);
-        //canvas.GetComponentInChildren<Image>().color = Color.blue;
-
-        // Text
-        GameObject myText = new GameObject();
-        myText.transform.parent = canvas.transform;
-        myText.name = "wibble";
-
-        Text text = myText.AddComponent<Text>();
-        text.text = "wobble";
-        text.color = Color.red;
-        text.fontSize = 100;
-
-        // Text position
-        RectTransform rectTransform = text.GetComponent<RectTransform>();
-        rectTransform.localPosition = new Vector3(0, 0, 0);
-        rectTransform.sizeDelta = new Vector2(400, 200);
-
-      Debug.Log(_results.Count);
-        Debug.Log(GetComponent<Canvas>());
+          NotificationController.Notify("No results returned from query!");
+          _results = new List<ScoredSegment>();
       }
+
+      _nResults = _results.Count;
+
+
+      /*Canvas canvas = gameObject.AddComponent<Canvas>();
+
+      gameObject.AddComponent<CanvasScaler>();
+      gameObject.AddComponent<GraphicRaycaster>();
+      GameObject panel = new GameObject("Panel");
+      panel.AddComponent<CanvasRenderer>();
+      panel.transform.SetParent(gameObject.transform, false);
+
+      Image image = panel.AddComponent<Image>();
+      image.color = Color.gray;
+      
+      // Text
+      GameObject myText = new GameObject();
+      myText.transform.parent = panel.transform;
+      myText.name = "wibble";
+
+      Text text = myText.AddComponent<Text>();
+      text.text = "wobble";
+      text.color = Color.red;
+      text.fontSize = 100;
+      
+      // Text position
+      RectTransform rectTransform = text.GetComponent<RectTransform>();
+      rectTransform.localPosition = new Vector3(0, 0, 0);
+      rectTransform.sizeDelta = new Vector2(400, 200);
+      */
+
+      gameObject.transform.position = new Vector3(0, 1.2f, 0.5f);
+
+      Scrollbar scrollbar = gameObject.GetComponentInChildren<Scrollbar>();
+      Debug.Log(scrollbar.size);
+
+      //GameObject scrollbar = GameObject.Find("/AdvancedGridQueryDisplay/Canvas/Panel/Scrollbar");
+
+      //get Text
+      TextMeshProUGUI titleText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+
+      titleText.text = "Advanced Grid (Displaying: " + _nResults + " Results)";
+
+      //get Panel
+      Transform gridPanel = gameObject.transform.Find("Canvas").transform.Find("Panel");
+     
+      if (gridPanel != null)
+      {
+        CreateResultObject(_results[0], gridPanel.gameObject);
+
+      }
+
     }
+
+    private void CreateResultObject(ScoredSegment result, GameObject panel)
+    {
+      // Determine position
+      //var index = _mediaDisplays.Count;
+      //var (position, rotation) = GetResultLocalPosRot(index);
+
+      var itemDisplay = Instantiate(mediaItemDisplay, Vector3.zero, Quaternion.identity, transform);
+
+      
+
+      var transform2 = itemDisplay.transform;
+
+      transform2.SetParent(panel.transform);
+      transform2.localPosition = new Vector3(0, 0, 0);
+
+      RectTransform rectTransform = itemDisplay.GetComponent<RectTransform>();
+      rectTransform.anchoredPosition = new Vector3(0, 0, 0);
+      
+
+      //transform2.localRotation = rotation;
+      // Adjust size
+      transform2.localScale *= 0.5f;
+
+      // Add to media displays list
+      _mediaDisplays.Add(itemDisplay);
+
+      itemDisplay.Initialize(result);
+
+      itemDisplay.gameObject.SetActive(true);
+    }
+  }
 }
